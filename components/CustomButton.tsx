0a058f7541cde1230607864d511d1ddefb7b359c
wowjdd133 <wowjddl133@naver.com>
2025-03-17 15:48:26 +0900

    [change] 버튼 리팩토링

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,36 +1,24 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { ButtonProps } from "@/types/type";
 
-const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
-  switch (variant) {
-    case "secondary":
-      return "bg-gray-500";
-    case "danger":
-      return "bg-red-500";
-    case "success":
-      return "bg-green-500";
-    case "outline":
-      return "bg-transparent border-neutral-300 border-[0.5px]";
-    default:
-      return "bg-[#0286ff]";
-  }
-};
-
-const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
-  switch (variant) {
-    case "primary":
-      return "text-black";
-    case "secondary":
-      return "text-gray-100";
-    case "danger":
-      return "text-red-100";
-    case "success":
-      return "text-green-100";
-
-    default:
-      return "text-white";
-  }
+const styles = {
+  baseButton:
+    "w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70",
+  bgVariants: {
+    primary: "bg-[#0286ff]",
+    secondary: "bg-gray-500",
+    danger: "bg-red-500",
+    success: "bg-green-500",
+    outline: "bg-transparent border-neutral-300 border-[0.5px]",
+  },
+  textVariants: {
+    default: "text-white",
+    primary: "text-black",
+    secondary: "text-gray-100",
+    danger: "text-red-100",
+    success: "text-green-100",
+  },
 };
 
 const CustomButton = ({
@@ -40,17 +28,17 @@ const CustomButton = ({
   textVariant = "default",
   IconLeft,
   IconRight,
-  className,
+  className = "",
   ...props
 }: ButtonProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      className={`${styles.baseButton} ${styles.bgVariants[bgVariant]} ${className}`}
       {...props}
     >
       {IconLeft && <IconLeft />}
-      <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+      <Text className={`text-lg font-bold ${styles.textVariants[textVariant]}`}>
         {title}
       </Text>
       {IconRight && <IconRight />}
